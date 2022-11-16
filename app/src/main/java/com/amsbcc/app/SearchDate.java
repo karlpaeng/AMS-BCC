a7f63ea9f0c0f371fbea104a3eb44934f449a2eb
karlpaeng <112166240+karlpaeng@users.noreply.github.com>
2022-11-16 10:01:46 +0800

    implemented a date picker funcionality

--- app/src/main/java/com/amsbcc/app/SearchDate.java
+++ app/src/main/java/com/amsbcc/app/SearchDate.java
@@ -2,13 +2,45 @@ package com.amsbcc.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
-public class SearchDate extends AppCompatActivity {
+import java.util.Calendar;
 
+public class SearchDate extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    Button datePickerButton;
+    String[] monthNames = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_date);
+
+        datePickerButton = (Button) findViewById(R.id.datePickerBtn);
+
+        datePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+    }
+    private void showDatePickerDialog(){
+        DatePickerDialog datePickDia = new DatePickerDialog(
+                this,
+                this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+        );
+        datePickDia.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        String dateSelected = monthNames[i1] + " " + i2 + ", " + i;
+        datePickerButton.setText(dateSelected);
     }
 }
