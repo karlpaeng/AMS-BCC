e91fbdb064ffd1f0d4c38d7c4cb63641c1bb1180
karlpaeng <112166240+karlpaeng@users.noreply.github.com>
2022-11-19 18:06:45 +0800

    wrote functions for sign in table at the DbHelper class

--- app/src/main/java/com/amsbcc/app/DBHelper.java
+++ app/src/main/java/com/amsbcc/app/DBHelper.java
@@ -1,10 +1,16 @@
 package com.amsbcc.app;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(@Nullable Context context) {
@@ -13,19 +19,57 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("" +
-                "CREATE TABLE scans (" +
+        sqLiteDatabase.execSQL("CREATE TABLE scans (" +
                 "scan_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "stud_id INT, " +
+                "stud_num INTEGER, " +
                 "name TEXT, " +
                 "date TEXT, " +
                 "time TEXT, " +
                 "log TEXT)"
         );
+        sqLiteDatabase.execSQL("CREATE TABLE students (" +
+                "stud_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "stud_name TEXT, " +
+                "stud_yr INTEGER, " +
+                "stud_course TEXT, " +
+                "stud_section TEXT, " +
+                "stud_contact TEXT)"
+        );
+        sqLiteDatabase.execSQL("CREATE TABLE sign_in (" +
+                "id INTEGER PRIMARY KEY, " +
+                "last_sign TEXT, " +
+                "status INTEGER, )"
+        );
+        addSigninRecord();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    }
+    public void addSigninRecord(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        String date = new SimpleDateFormat("yyyy-MMM-dd", Locale.getDefault()).format(new Date());
+        cv.put("id", 1);
+        cv.put("last_sign", date);
+        cv.put("status", 0);
+
+        long i = db.insert("sign_in", null, cv);
+
+        db.close();
+    }
+    public void updateSigninRecord(int value){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String date = new SimpleDateFormat("yyyy-MMM-dd", Locale.getDefault()).format(new Date());
+        db.execSQL("UPDATE sign_in SET last_sign = '" + date + "', status = " + value + " WHERE id = 1;");
+        db.close();
+
+    }
+    public int getSigninStatus(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT status FROM sign_in WHERE id = 1;", null);
+        cursor.moveToFirst();
+        return cursor.getInt(0);
     }
 }
