e2b12fc23b224da94d984250f382075af232fc4d
karlpaeng <112166240+karlpaeng@users.noreply.github.com>
2023-01-27 14:10:51 +0800

    added working buttons on view records fragment

--- app/src/main/java/com/amsbcc/app/ViewFrag.java
+++ app/src/main/java/com/amsbcc/app/ViewFrag.java
@@ -1,5 +1,6 @@
 package com.amsbcc.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +16,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ViewFrag extends Fragment {
+    View v;
 
+    Button searchId, searchClass, searchDate;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +63,32 @@ public class ViewFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view, container, false);
+        v = inflater.inflate(R.layout.fragment_view, container, false);
+        searchId = v.findViewById(R.id.searchIDNew);
+        searchClass = v.findViewById(R.id.searchClassNew);
+        searchDate = v.findViewById(R.id.searchDateNew);
+
+        searchId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent((MainActivity) getActivity(), SearchID.class);
+                startActivity(intent);
+            }
+        });
+        searchClass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent((MainActivity) getActivity(), SearchClass.class);
+                startActivity(intent);
+            }
+        });
+        searchDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent((MainActivity) getActivity(), SearchDate.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
